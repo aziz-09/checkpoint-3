2d3a2528222cedfed631a852365fd3e23699dcda
aziz-09 <59797946+aziz-09@users.noreply.github.com>
2020-02-23 17:44:01 +0100

    Add files via upload

---
+++ pp/main.js
@@ -0,0 +1,25 @@
+function makebold()
+{ if(document.getElementById("txt").style.fontWeight=="normal")
+document.getElementById("txt").style.fontWeight="bold"
+else{ document.getElementById("txt").style.fontWeight="normal"}
+}
+
+function makeitalic(){
+ if(document.getElementById("txt").style.fontStyle=="normal")
+ document.getElementById("txt").style.fontStyle="italic"
+ else{document.getElementById("txt").style.fontStyle="normal"}
+}
+
+function makeunder(){
+    if(document.getElementById("txt").style.textDecoration=="none")
+    document.getElementById("txt").style.textDecoration="underline"
+    else{document.getElementById("txt").style.textDecoration="none"}
+
+}
+
+function changeFontSize(fontSize){
+    document.getElementById("txt").style.fontSize=fontSize.value+"px"
+}
+function changeFontFamily(fontFamily){
+    document.getElementById("txt").style.fontFamily=fontFamily.value
+}
